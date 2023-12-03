29648358808245260e07ae6c854279658112e64b
USER <msobanakram7@gmail.com>
2023-12-03 21:42:17 +0500

    after deletion function

--- avl.cpp
+++ avl.cpp
@@ -95,8 +95,89 @@ Node* insert(Node* root, int key)
     }
     return root;
 }
+Node* getPre(Node* root)
+{
+    root = root -> left;
+    if (root == nullptr)
+    {
+        return root;
+    }
+    while (root -> right != nullptr)
+    {
+        root = root -> right;
+    }
+    return root;
+}
 Node* deletion(Node* root, int key)
 {
+    // First Delete node using standard BST delete Function
+    Node* pre;
+    if (root == nullptr)
+    {
+        return root;
+    }
+    else if (root -> left == nullptr && root -> right == nullptr)
+    {
+        if (root -> val == key)
+        {
+            root = nullptr;
+        }
+        return root;
+    }
+
+    if (key < root -> val)
+    {
+        root -> left = deletion(root -> left, key);
+    }
+    else if (key > root -> val)
+    {
+        root -> right = deletion(root -> right, key);
+    }
+    else
+    {
+        pre = getPre(root);
+        if (pre != nullptr)
+        {
+            root -> val = pre -> val;
+            root -> left = deletion(root -> left, pre -> val);
+        }
+        else if (root -> right != nullptr)
+        {
+            return root -> right;
+        }
+        else if (root -> right == nullptr)
+        {
+            return nullptr;
+        }
+    }
+        // step 2 Update height of the current tree
+        root -> height = 1 + max(getHeight(root -> left), getHeight(root -> right));
+    int bf = getBalanceFactor(root);
+
+    // Left Left Case
+    if (bf > 1 && getBalanceFactor(root -> left) >= 0)
+    {
+        return rightRotate(root);
+    }
+    // Right Right Case
+    if (bf < -1 && getBalanceFactor(root -> right) <= 0)
+    {
+        return leftRotate(root);
+    }
+    // Left Right Case
+    if (bf > 1 && getBalanceFactor(root -> left) < 0)
+    {
+        root -> left = leftRotate(root -> left);
+        return rightRotate(root);
+    }
+    // Right Left Case
+    if (bf < -1 && getBalanceFactor(root -> right) > 0)
+    {
+        root -> right = rightRotate(root -> right);
+        return leftRotate(root);
+    }
+
+        return root;
     
 }
 void preOrder(Node* root)
@@ -115,11 +196,13 @@ void preOrder(Node* root)
 int main()
 {
     Node* root = new Node(9);
+    Node* temp = root;
     root = insert(root, 8);
     root = insert(root, 15);
     root = insert(root, 11);
     root = insert(root, 20);
     root = insert(root, 17);
+    deletion(temp, 8);
     preOrder(root);
 
 }
